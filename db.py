120d97428b2475cbd84acc64223b65fc2af90cf4
tiaineno <olli.vj.tiainen@gmail.com>
2024-04-29 18:56:37 +0300

    Fix Fly.io config

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+import psycopg2
 from dotenv import load_dotenv
 load_dotenv()
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
 db = SQLAlchemy(app)
